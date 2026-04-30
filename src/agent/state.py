8c8d4258d48ba6bd76a250c86b4c087b115f9747
Ezekiel Thawngcung <Ezekiel.Thawngcung+prft@perficient.com>
2026-04-30 08:07:59 -0500

    simplify states

--- src/agent/state.py
+++ src/agent/state.py
@@ -2,6 +2,7 @@
 from pydantic import BaseModel, Field
 from typing import Literal, Optional, List, Annotated
 from langchain.messages import SystemMessage, HumanMessage, AIMessage
+from langgraph.graph import MessagesState
 
 import operator
 
@@ -12,6 +13,8 @@ Urgency = Literal[1, 2, 3] # (1|2|3, High|Medium|Low)
 Priority = Literal[1, 2, 3, 4, 5] # "1 - Critical", "2 - High", "3 - Moderate", "4 - Low", "5 - Planning"
 # The extent to which an incident affects the business
 Impact = Literal[1, 2, 3]  # (1|2|3, High|Medium|Low)
+
+Breaker_State = Literal["OPEN", "CLOSED", "HALF-OPEN"]
 
 class EmailState(BaseModel):
     sender_email: str = Field(..., description="Email address of the sender")
@@ -24,6 +27,7 @@ class EmailClassification(BaseModel):
     urgency: Urgency = Field(..., description="Urgency level of the issue")
     category: Category = Field(..., description="Category of the issue")
     summary: str = Field(..., description="A brief summary of the issue described in the email")
+    body: str
 
 class ServiceNowIncident(BaseModel):
     short_description: str = Field(..., description="Short description of the incident")
@@ -33,6 +37,8 @@ class ServiceNowIncident(BaseModel):
     caller_id: Optional[str] = Field(None, description="Identifier for the caller (user reporting the incident)")
 
 class AgentState(BaseModel):
-    messages: Annotated[List[HumanMessage | AIMessage | SystemMessage], Field(default_factory=list, description="List of email interactions related to the user's issue"), operator.add]
-    draft_ticket: Optional[ServiceNowIncident] = Field(None, description="Draft response being composed by the agent")
-    comment_history: Annotated[Optional[List[str]], Field(default_factory=list, description="List of comments added to the incident ticket by the agent"), operator.add]
+    messages: Annotated[List[str | EmailClassification], Field(None, description="conversation history"), operator.add]
+    # draft_ticket: Optional[ServiceNowIncident] = Field(None, description="Draft response being composed by the agent")
+    # comment_history: Annotated[Optional[List[str]], Field(None, description="List of comments added to the incident ticket by the agent"), operator.add]
+    classification: str | None
+    # breaker_state: Breaker_State = Field(default="CLOSED", description="state of any risky operation")
