fbec5fbc16e92388a5fef2f2f667490a2e64c892
Ezekiel Thawngcung <Ezekiel.Thawngcung+prft@perficient.com>
2026-04-30 08:05:43 -0500

    make graph workflow reusable for independent isolated state

--- src/agent/graph.py
+++ src/agent/graph.py
@@ -10,27 +10,26 @@ from agent.nodes import analyze_issue, analyze_issue, approval, classification, 
 from agent.state import AgentState
 from dotenv import load_dotenv
 
-graph = StateGraph(AgentState)
-# add nodes
-graph.add_node("input_guardrail", input_guardrail)
-graph.add_node("classification", classification)
-graph.add_node("analyze_issue", analyze_issue)
-graph.add_node("output_guardrail", output_guardrail)
-graph.add_node("approval", approval)
-graph.add_node("create_ticket", create_ticket)
-graph.add_node("send_slack_notification", send_slack_notification)
+async def workflow():
+    graph = StateGraph(AgentState)
+    # add nodes
+    graph.add_node("input_guardrail", input_guardrail)
+    graph.add_node("classification", classification)
+    graph.add_node("analyze_issue", analyze_issue)
+    graph.add_node("output_guardrail", output_guardrail)
+    graph.add_node("approval", approval)
+    graph.add_node("create_ticket", create_ticket)
+    graph.add_node("send_slack_notification", send_slack_notification)
 
-# add edges
-graph.add_edge(START, "input_guardrail")
-graph.add_edge("input_guardrail", "classification")
-graph.add_edge("classification", "analyze_issue")
-graph.add_edge("analyze_issue", "output_guardrail")
-graph.add_edge("output_guardrail", "approval")
-graph.add_edge("approval", "create_ticket")
-graph.add_edge("create_ticket", "send_slack_notification")
-graph.add_edge("send_slack_notification", END)
+    # add edges
+    graph.add_edge(START, "input_guardrail")
+    graph.add_edge("input_guardrail", "classification")
+    graph.add_edge("classification", "analyze_issue")
+    graph.add_edge("analyze_issue", "output_guardrail")
+    graph.add_edge("output_guardrail", "approval")
+    graph.add_edge("approval", "create_ticket")
+    graph.add_edge("create_ticket", "send_slack_notification")
+    graph.add_edge("send_slack_notification", END)
 
-# compile graph
-graph.compile()
-
-# invoke graph
+    # compile graph and return    
+    return graph.compile()
